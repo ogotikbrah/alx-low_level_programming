8116b7ef5be7a2c2e531ae188d826de6a2c0e0e9
ogotikbrah <kengaraogitiabro@gmail.com>
2023-03-14 10:50:29 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,42 +1,43 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
-* str_concat - get ends of input and add together for size
-* @s1: input one to concat
-* @s2: input two to concat
-* Return: concat of s1 and s2
-*/
+ * str_concat - concatenates two strings
+ * @s1: string to be joined to
+ * @s2: string to join s1
+ *
+ * Return: pointer to newly allocated space in memory
+ *         containing concatenated string , or NULL if error
+ *         in concatenating
+ */
 
 char *str_concat(char *s1, char *s2)
-
 {
-	char *conct;
-	int i, ci;
+	char *join;
+	int copy = 0;
+	int length = 0;
+	int i;
 
-	if(s1 == NULL);
+	if (s1 == NULL)
 		s1 = "";
-	if(s2 == NULL);
+
+	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-	if (conct == NULL)
+
+	for (i = 0; s1[i] || s2[i]; i++)
+		length++;
+
+	join = malloc(sizeof(char) * length);
+
+	if (join == NULL)
 		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
-	}
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+
+	for (i = 0; s1[i]; i++)
+		join[copy++] = s1[i];
+
+	for (i = 0; s2[i]; i++)
+		join[copy++] = s2[i];
+
+	return (join);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
-* free_grid - frees 2d array
-* @grid: 2d grid
-* @height: height dimension of grid
-* Description: frees memory of grid
-* Return: nothing
-*
-*/
-void free_grid(int **grid, int height);
+ * free_grid - frees a 2Dimensional grid previously
+ *             created by my alloc_grid function.
+ * @grid: grid to free
+ * @height: height of grid
+ *
+ * Return: void
+ */
 
+void free_grid(int **grid, int height)
 {
-
 	int i;
 
+	if (grid == NULL || height <= 0)
+		return;
+
 	for (i = 0; i < height; i++)
-	{
 		free(grid[i]);
-	}
+
 	free(grid);
 }
