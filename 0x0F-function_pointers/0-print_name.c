6f35d2932430ef19fbdf76a5f1a38b7cdc6940db
kbrah <123351058+ogotikbrah@users.noreply.github.com>
2023-03-23 02:13:06 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,46 +1,15 @@
+#include "function_pointers.h"
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - Prints the opcodes of its main function
- * @argc: number of arguments supplied to the program.
- * @argv: array of pointers to the arguments.
- *
- * Return: 0.
- */
-int main(int argc, char *argv[])
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
+void print_name(char *name, void (*f)(char *))
 {
-	int b, i;
-	int (*address)(int, char **) = main;
-	unsigned char opcode;
+	if (name == NULL || f == NULL)
+		return;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-
-	b = atoi(argv[1]);
-
-	if (b < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	for (i = 0; i < b; i++)
-	{
-		opcode = *(unsigned char *)address;
-		printf("%.2x", opcode);
-
-		if (i == b - 1)
-			continue;
-		printf(" ");
-
-		address++;
-	}
-
-	printf("\n");
-
-	return (0);
+	f(name);
 }
